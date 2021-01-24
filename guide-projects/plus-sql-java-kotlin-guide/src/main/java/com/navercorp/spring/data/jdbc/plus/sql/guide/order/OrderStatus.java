097b2fcd359871be14d4584cc04131feaafb58c5
mhyeon-lee <mhyeon.lee@navercorp.com>
2021-01-24 22:38:41 +0900

    Update copyright year to 2021.
    (cherry picked from commit 32ec3b3caa269443e9bfafdaade3982e159ce463)

--- guide-projects/plus-sql-java-kotlin-guide/src/main/java/com/navercorp/spring/data/jdbc/plus/sql/guide/order/OrderStatus.java
+++ guide-projects/plus-sql-java-kotlin-guide/src/main/java/com/navercorp/spring/data/jdbc/plus/sql/guide/order/OrderStatus.java
@@ -1,7 +1,7 @@
 /*
  * Spring JDBC Plus
  *
- * Copyright 2020-present NAVER Corp.
+ * Copyright 2020-2021 NAVER Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
