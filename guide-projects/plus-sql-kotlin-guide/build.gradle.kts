2ec9654b3f21a310f7b767b582e0edd2ba7186ee
Taeik Lim <sibera21@gmail.com>
2024-06-03 09:42:03 +0900

    Use convention in gradle project structure (#147)
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

--- guide-projects/plus-sql-kotlin-guide/build.gradle.kts
+++ guide-projects/plus-sql-kotlin-guide/build.gradle.kts
@@ -1,28 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-buildscript {
-    repositories {
-        maven(url = "https://plugins.gradle.org/m2/")
-    }
-    dependencies {
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
-    }
-}
-
 plugins {
-    val kotlinVersion = "1.9.20"
-
+    id("spring.jdbc.plus.spring-bom-conventions")
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.spring") version kotlinVersion
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
 }
 
-repositories {
-    mavenCentral()
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+    kotlinOptions.javaParameters = true
 }
 
 dependencies {
-
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation(project(":spring-boot-starter-data-jdbc-plus-sql"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -35,9 +25,4 @@ dependencies {
     implementation("org.springframework.data:spring-data-commons")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-    kotlinOptions.javaParameters = true
 }
