0ad08a0b9fc35fe2fcdbf468d86974107dbd3d5c
yd.2 <yd.2@navercorp.com>
2025-12-01 16:27:20 +0900

    Introduce  and extract factory methods used from .

--- spring-data-jdbc-plus-repository/src/main/java/com/navercorp/spring/data/jdbc/plus/repository/config/AbstractJdbcPlusConfiguration.java
+++ spring-data-jdbc-plus-repository/src/main/java/com/navercorp/spring/data/jdbc/plus/repository/config/AbstractJdbcPlusConfiguration.java
@@ -3,12 +3,11 @@ package com.navercorp.spring.data.jdbc.plus.repository.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.core.convert.DataAccessStrategy;
 import org.springframework.data.jdbc.core.convert.JdbcConverter;
+import org.springframework.data.jdbc.core.convert.QueryMappingConfiguration;
 import org.springframework.data.jdbc.core.dialect.JdbcDialect;
 import org.springframework.data.jdbc.core.mapping.JdbcMappingContext;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-
-import com.navercorp.spring.data.jdbc.plus.support.convert.JdbcPlusDataAccessStrategyFactory;
 
 public class AbstractJdbcPlusConfiguration extends AbstractJdbcConfiguration {
 
@@ -27,13 +26,11 @@ public class AbstractJdbcPlusConfiguration extends AbstractJdbcConfiguration {
 		JdbcMappingContext context,
 		JdbcDialect dialect
 	) {
-		DataAccessStrategy delegate = super.dataAccessStrategyBean(operations, jdbcConverter, context, dialect);
-
-		return new JdbcPlusDataAccessStrategyFactory(
-			delegate,
-			jdbcConverter,
+		return JdbcPlusConfiguration.createDataAccessStrategy(
 			operations,
+			jdbcConverter,
+			QueryMappingConfiguration.EMPTY,
 			dialect
-		).create();
+		);
 	}
 }
